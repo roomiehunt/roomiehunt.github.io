59964d4f9d830d088e2239a873297b89d839654c
dio <abrahamdio@gmail.com>
2015-10-26 00:09:10 -0500

    clean up

--- shroomme/shroomme/messaging/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import messages,threads,thread_messages
-
-
-admin.site.register(messages)
-admin.site.register(threads)
-
-admin.site.register(thread_messages)
-# Register your models here.
